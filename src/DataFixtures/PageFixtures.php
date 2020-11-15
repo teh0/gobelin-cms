db02aea56d20751be505c8b7ab9e7c3aff2e4a7a
Théo <tochampion38@gmail.com>
2020-11-15 20:06:19 +0100

    🗃  Add TagFixtures

--- src/DataFixtures/PageFixtures.php
+++ src/DataFixtures/PageFixtures.php
@@ -13,6 +13,7 @@ class PageFixtures extends BaseFixture implements DependentFixtureInterface
     const TOTAL_NUMBER_PAGES = 50;
     const PREFIX_REFERENCE = 'page';
     const NUMBER_CATEGORY_BY_PAGE = 3;
+    const NUMBER_TAG_BY_PAGE = 3;
 
     public function load(ObjectManager $manager)
     {
@@ -22,11 +23,23 @@ class PageFixtures extends BaseFixture implements DependentFixtureInterface
             $page->setContent($this->faker->randomHtml());
             $page->setAuthor($this->getReference(UserFixtures::PREFIX_REFERENCE . '-1'));
             $this->setCategories($page);
+            $this->setTags($page);
             $this->addReference(self::PREFIX_REFERENCE . "-$i", $page);
             $manager->persist($page);
         }
 
         $manager->flush();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class,
+            CategoryFixtures::class
+        ];
     }
 
 
@@ -43,14 +56,16 @@ class PageFixtures extends BaseFixture implements DependentFixtureInterface
         }
     }
 
-    /**
-     * @inheritDoc
-     */
-    public function getDependencies()
+    private function setTags(Page $page): void
     {
-        return [
-            UserFixtures::class,
-            CategoryFixtures::class
-        ];
+        $tagReferences = $this->getRandomRelationReferences(
+            TagFixtures::PREFIX_REFERENCE,
+            TagFixtures::TOTAL_NUMBER_TAGS,
+            self::NUMBER_TAG_BY_PAGE
+        );
+
+        foreach ($tagReferences as $tagReference) {
+            $page->addTag($this->getReference($tagReference));
+        }
     }
 }
